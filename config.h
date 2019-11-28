a4a2c35b86f061f472a0e148dc9ed718dccf04d4
Youmin Chen <chenym16@mails.tsinghua.edu.cn>
2019-11-29 00:15:40 +0800

    sync

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
-#define THREAD_CNT 1
-#define CC_ALG WAIT_DIE
-#define ZIPF_THETA 0.5
-#define READ_PERC 0
-#define WRITE_PERC 1
+#define THREAD_CNT 36
+#define CC_ALG HEKATON
+#define ZIPF_THETA 0.99
+#define READ_PERC 1
+#define WRITE_PERC 0
 /***********************************************/
 // Simulation + Hardware
 /***********************************************/
@@ -112,7 +112,7 @@
 // max number of rows touched per transaction
 #define MAX_ROW_PER_TXN				16
 #define QUERY_INTVL 				1UL
-#define MAX_TXN_PER_PART 			1000000
+#define MAX_TXN_PER_PART 			100000
 #define FIRST_PART_LOCAL 			true
 #define MAX_TUPLE_SIZE				1024 // in bytes
 // ==== [YCSB] ====
